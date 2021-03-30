fb93aef4a16351e9be02f0aa419424a2589c5d80
OnixFang <twily.dev@outlook.com>
2021-03-30 18:24:49 -0400

    Create action creators

--- src/index.js
+++ src/index.js
@@ -1,32 +1,12 @@
 import store from './store';
+import { bugAdded, bugRemoved } from './actions';
 
-const action1 = {
-  type: 'bugAdded',
-  payload: { description: 'Ivern turns invisible on wall.' },
-};
+const unsubscribe = store.subscribe(() => {
+  console.log('Store changed!', store.getState());
+});
 
-const action2 = {
-  type: 'bugAdded',
-  payload: { description: 'Viego gets 1000 stacks on double kill.' },
-};
-
-const action3 = {
-  type: 'bugAdded',
-  payload: {
-    description: "Nunu's snowball does not dissapear after hitting champ.",
-  },
-};
-
-const action4 = {
-  type: 'bugRemoved',
-  payload: {
-    id: 3,
-  },
-};
-
-store.dispatch(action1);
-store.dispatch(action2);
-store.dispatch(action3);
-store.dispatch(action4);
-
-console.log(store.getState());
+store.dispatch(bugAdded('Ivern turns invisible on wall.'));
+store.dispatch(bugAdded('Viego gets 1000 stacks on double kill.'));
+// unsubscribe();
+store.dispatch(bugAdded("Nunu's snowball does not dissapear after hitting champ."));
+store.dispatch(bugRemoved(2));
