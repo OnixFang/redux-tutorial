c0cbbe687882ed19c5b47ff201955c7020c562b1
OnixFang <twily.dev@outlook.com>
2021-03-30 17:59:31 -0400

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,12 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: './index.js',
+    filename: 'app.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
   },
-  mode: 'development',
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     port: 9000,
   },
+  mode: 'development',
 };
