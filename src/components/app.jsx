87cc40614107e0aa0977a177435f3c1f6913fba1
OnixFang <twily.dev@outlook.com>
2021-03-30 22:08:19 -0400

    Setup basic react app structure

---
+++ src/components/app.jsx
@@ -0,0 +1,17 @@
+// import store from './store';
+// import { bugAdded, bugRemoved, bugResolved } from './actions';
+
+// const unsubscribe = store.subscribe(() => {
+//   console.log('Store changed!', store.getState());
+// });
+
+// store.dispatch(bugAdded('Ivern turns invisible on wall.'));
+// store.dispatch(bugAdded('Viego gets 1000 stacks on double kill.'));
+// // unsubscribe();
+// store.dispatch(bugAdded("Nunu's snowball does not dissapear after hitting champ."));
+// store.dispatch(bugRemoved(2));
+// store.dispatch(bugResolved(3));
+
+export default function App() {
+  return <h1>HELLO!</h1>;
+}
